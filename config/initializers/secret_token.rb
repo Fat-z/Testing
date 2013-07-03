f842704f8d57b995821d82ed8c9290eb04c1aba2
Fat-z <2641580591@qq.com>
2013-07-03 10:32:27 +0800

    initalize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '941487ff4e58cfa50b032be337c9ec26f2e1f80e3769b8725d7faddc800b641b75cf2724face28f2a037fea994d2ba71342aca2a0b9d7a4770fb4424976ae4ba'
